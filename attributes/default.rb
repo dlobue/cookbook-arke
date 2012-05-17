250aef6097181dd77821a98fc0c93e25544291c1
Dominic LoBue <dominic.lobue@gmail.com>
2012-05-16 21:11:32 -0700

    initial commit

---
+++ attributes/default.rb
@@ -0,0 +1,22 @@
+
+default.arke.plugin_dir = '/etc/arke/plugins'
+default.arke.config_file = '/etc/arke/arke.conf'
+
+default.arke.backend = 'http'
+default.arke.backend_host = 'localhost'
+default.arke.backend_port = Promise.new {
+    (attribute?("clio") and clio.attribute?("port")) ? clio.port : 64000
+}
+
+default.arke.region_limit = true
+
+default.arke.has_mongodb = false
+
+default.arke.mongodb_host = 'localhost'
+default.arke.mongodb_port = Promise.new { mongodb.port }
+
+default.arke.has_postgres_repl = false
+default.arke.pg_repl_hosts = 'localhost'
+default.arke.pg_repl_port = 5432
+default.arke.pg_repl_user = 'postgres'
+
---
+++ recipes/default.rb
@@ -0,0 +1,25 @@
+
+package "python-arke" do
+    action :upgrade
+    notifies :stop, "service[arke]", :immediately
+end
+
+directory node.arke.plugin_dir do
+    action :create
+    mode "0755"
+    recursive true
+end
+
+template node.arke.config_file do
+    source "arke.conf.erb"
+    mode 0644
+    notifies :stop, "service[arke]", :immediately
+end
+
+service "arke" do
+    provider Chef::Provider::Service::Upstart
+    supports [:status, :restart, :reload]
+    action :start
+end
+
+
